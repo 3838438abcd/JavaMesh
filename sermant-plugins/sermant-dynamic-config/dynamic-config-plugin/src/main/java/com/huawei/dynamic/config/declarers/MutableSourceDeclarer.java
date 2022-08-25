a7f243f40a69256f9ecf43568bcd4ef8ea0a7ea4
justforstudy <2167005981@qq.com>
2022-08-25 09:18:27 +0800

    基于动态方式提升动态配置优先级，确保动态关闭场景新的配置中心配置生效
    添加自动化测试maven依赖缓存
    
    review意见修改

---
+++ sermant-plugins/sermant-dynamic-config/dynamic-config-plugin/src/main/java/com/huawei/dynamic/config/declarers/MutableSourceDeclarer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2022-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.huawei.dynamic.config.declarers;
+
+import com.huawei.dynamic.config.interceptors.MutableSourceInterceptor;
+
+import com.huaweicloud.sermant.core.plugin.agent.declarer.AbstractPluginDeclarer;
+import com.huaweicloud.sermant.core.plugin.agent.declarer.InterceptDeclarer;
+import com.huaweicloud.sermant.core.plugin.agent.matcher.ClassMatcher;
+import com.huaweicloud.sermant.core.plugin.agent.matcher.MethodMatcher;
+
+/**
+ * 针对addFirst方法拦截, 当动态源配置中心时, 拦截禁止添加源配置中心配置源, 阻止配置生效
+ *
+ * @author zhouss
+ * @since 2022-04-08
+ */
+public class MutableSourceDeclarer extends AbstractPluginDeclarer {
+    private static final String ENHANCE_CLASS =
+            "org.springframework.core.env.MutablePropertySources";
+
+    private static final String INTERCEPTOR_CLASS = MutableSourceInterceptor.class.getCanonicalName();
+
+    @Override
+    public ClassMatcher getClassMatcher() {
+        return ClassMatcher.nameEquals(ENHANCE_CLASS);
+    }
+
+    @Override
+    public InterceptDeclarer[] getInterceptDeclarers(ClassLoader classLoader) {
+        return new InterceptDeclarer[]{
+                InterceptDeclarer.build(MethodMatcher.nameEquals("addFirst").and(MethodMatcher.paramCountEquals(1)),
+                        INTERCEPTOR_CLASS)
+        };
+    }
+}
