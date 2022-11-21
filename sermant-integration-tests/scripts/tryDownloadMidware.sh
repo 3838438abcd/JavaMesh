cfe232369c3c0f71d8f4b9ac817799a615d6c0dc
justforstudy <2167005981@qq.com>
2022-11-21 14:32:26 +0800

    版本替换

--- sermant-integration-tests/scripts/tryDownloadMidware.sh
+++ sermant-integration-tests/scripts/tryDownloadMidware.sh
@@ -29,6 +29,10 @@ ZK_FILE_NAME=apache-zookeeper-3.6.3-bin.tar.gz
 NACOS_ADDRESS=https://github.com/alibaba/nacos/releases/download/1.4.2/nacos-server-1.4.2.tar.gz
 NACOS_FILE_NAME=nacos-server-1.4.2.tar.gz
 
+#======================================Service Center配置======================================
+SERVICE_CENTER_ADDRESS=https://github.com/apache/servicecomb-service-center/releases/download/v2.1.0/apache-servicecomb-service-center-2.1.0-linux-amd64.tar.gz
+SERVICE_CENTER_FILE_NAME=apache-servicecomb-service-center-2.1.0-linux-amd64.tar.gz
+
 #重试次数
 TRY_TIMES=3
 
@@ -65,6 +69,9 @@ if [ $midleware == "zk" ];then
 elif [ $midleware == "nacos" ]; then
   ADDRESS=$NACOS_ADDRESS
   FILE_NAME=$NACOS_FILE_NAME
+elif [ $midleware == "sc" ]; then
+  ADDRESS=SERVICE_CENTER_ADDRESS
+  FILE_NAME=SERVICE_CENTER_FILE_NAME
 else
   ADDRESS=$CSE_ADDRESS
   FILE_NAME=$CSE_FILE_NAME
